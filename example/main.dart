f144f6bb9f07b6e8ceedb9d0b1ccf51b7c82c853
wurikiji <wurikiji@gmail.com>
2019-05-27 22:19:05 +0900

    Modify example

--- example/main.dart
+++ example/main.dart
@@ -84,3 +84,27 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
+
+class SecondScreen extends StatefulWidget {
+  SecondScreen({Key key}) : super(key: key);
+
+  _SecondScreenState createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: EasyStatefulBuilder(
+          identifier: 'counter2',
+          builder: (context, count) {
+            return Center(
+              child: Text("Count2 is still $count"),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
